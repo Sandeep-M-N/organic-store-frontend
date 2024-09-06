b303f0371610960460c0304a6bb03446b0a621b6
basilahamed07 <basilahamed46@gmail.com>
2024-09-06 18:45:53 +0530

    @basil upload admin panal proccesing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,45 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// admin coponents
+import { AdminPageComponent } from './admin_panal/admin-page/admin-page.component';
+import { CategoriesComponent } from './admin_panal/categories/categories.component';
+import { CoupondComponent } from './admin_panal/coupond/coupond.component';
+import { FeedbackComponent } from './admin_panal/feedback/feedback.component';
+import { OrderDetailsComponent } from './admin_panal/order-details/order-details.component';
+import { ShippingComponent } from './admin_panal/shipping/shipping.component';
+import { UseraddComponent } from './admin_panal/useradd/useradd.component';
+
+
+
+
+const routes: Routes = [
+
+  // {
+  //   path: 'dashboard', component: AdminDashboardComponent, children: [
+  //     { path: 'users', component: UsersComponent },
+  //     { path: 'edituser/:id', component: EdituserComponent },
+  //     {path:'rooms',component:RoomListComponent},
+  //     { path: 'rooms/add', component: AddRoomComponent },
+  //     {path:'roomcard',component:RoomCardComponent},
+  //     { path: 'feedback', component: FeedbackComponent },
+  //     { path: '', redirectTo: 'users', pathMatch: 'full' }
+  //   ]
+  // }, 
+
+  {
+   path:"admindash", component: AdminPageComponent, children:[
+    {path:"users", component:UseraddComponent},
+    {path:"categories", component:CategoriesComponent},
+    {path:"coupond", component:CoupondComponent},
+    {path:"feedback", component:FeedbackComponent},
+    {path:"order_details", component:OrderDetailsComponent},
+    {path:"shipping", component:ShippingComponent},
+   ]
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutPageComponent } from './content/about-page/about-page.component';
+import { AdminPageComponent } from './admin_panal/admin-page/admin-page.component';
+import { UseraddComponent } from './admin_panal/useradd/useradd.component';
+import { OrderDetailsComponent } from './admin_panal/order-details/order-details.component';
+import { CategoriesComponent } from './admin_panal/categories/categories.component';
+import { CoupondComponent } from './admin_panal/coupond/coupond.component';
+import { ShippingComponent } from './admin_panal/shipping/shipping.component';
+import { FeedbackComponent } from './admin_panal/feedback/feedback.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutPageComponent,
+    AdminPageComponent,
+    UseraddComponent,
+    OrderDetailsComponent,
+    CategoriesComponent,
+    CoupondComponent,
+    ShippingComponent,
+    FeedbackComponent
   ],
   imports: [
     BrowserModule,
